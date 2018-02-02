912ac4122a791dc025cc45d41f168f4eab3a4a6c
Artem Shcheglov <stiw00@bk.ru>
2018-02-02 20:31:25 +0300

    add other example contract

---
+++ my_contract.sol
@@ -0,0 +1,183 @@
+pragma solidity ^0.4.11;
+
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+    function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+        uint256 c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal constant returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal constant returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
+
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+    address public owner;
+
+    /**
+     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+     * account.
+     */
+    function Ownable() {
+        owner = msg.sender;
+    }
+
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+
+    /**
+     * @dev Allows the current owner to transfer control of the contract to a newOwner.
+     * @param newOwner The address to transfer ownership to.
+     */
+    function transferOwnership(address newOwner) onlyOwner {
+        require(newOwner != address(0));
+        owner = newOwner;
+    }
+
+}
+
+
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/179
+ */
+contract ERC20Basic {
+    uint256 public totalSupply;
+    function balanceOf(address who) constant returns (uint256);
+    function transfer(address to, uint256 value) returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+    function allowance(address owner, address spender) constant returns (uint256);
+    function transferFrom(address from, address to, uint256 value) returns (bool);
+    function approve(address spender, uint256 value) returns (bool);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract PoStocken is ERC20,Ownable {
+    using SafeMath for uint256;
+
+    string public name = "PoStoken";
+    string public symbol = "PST";
+    uint public decimals = 18;
+
+    mapping (address => mapping (address => uint)) allowed;
+    mapping (address => uint) balances;
+
+    function PoSToken() {
+        totalSupply = 10**24; // 1 mil.
+        balances[this] = totalSupply;
+    }
+
+     modifier onlyPayloadSize(uint size) {
+        require(msg.data.length >= size + 4);
+        _;
+    }
+
+    function BuyTocken() payable returns (bool) {
+        require (msg.value != 0);
+
+        var numTokens = msg.value;
+        totalSupply = totalSupply.add(numTokens);
+        balances[this] = balances[this].sub(numTokens);
+        balances[msg.sender] = balances[msg.sender].add(numTokens);
+
+        Transfer(this, msg.sender, numTokens);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint _value) onlyPayloadSize(3 * 32)returns (bool) {
+        require(_to != address(0));
+
+        var _allowance = allowed[_from][msg.sender];
+
+        balances[_from] = balances[_from].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        allowed[_from][msg.sender] = _allowance.sub(_value);
+        Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) returns (bool) {
+        require((_value == 0) || (allowed[msg.sender][_spender] == 0));
+
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+        return allowed[_owner][_spender];
+    }
+
+    function transfer(address _to, uint256 _value) onlyPayloadSize(2*32) returns (bool) {
+        if (msg.sender == _to)
+            return mint();
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function batchTransfer(address[] _recipients, uint[] _values) onlyOwner returns (bool) {
+        require( _recipients.length > 0 && _recipients.length == _values.length);
+
+        uint total = 0;
+        for(uint i = 0; i < _values.length; i++) {
+            total = total.add(_values[i]);
+        }
+        require(total <= balances[msg.sender]);
+
+        for(uint j = 0; j < _recipients.length; j++) {
+            balances[_recipients[j]] = balances[_recipients[j]].add(_values[j]);
+            Transfer(msg.sender, _recipients[j], _values[j]);
+        }
+
+        balances[msg.sender] = balances[msg.sender].sub(total);
+        return true;
+    }
+}
--- smart_contract.sol
+++ smart_contract.sol
@@ -1,1 +1,75 @@
 pragma solidity ^0.4.11;
+
+contract Owned {
+
+    address public owner;
+
+    function owned() public payable {
+        owner = msg.sender;
+    }
+    
+    modifier onlyOwner {
+        require(owner == msg.sender);
+        _;
+    }
+
+    function changeOwner(address _owner) onlyOwner public {
+        owner = _owner;
+    }
+}
+
+contract Crowdsale is Owned {
+    
+    uint256 public totalSupply;
+    mapping (address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    function Crowdsale() public payable Owned() {
+        totalSupply = 21000000;
+        balanceOf[this] = 20000000;
+        balanceOf[owner] = totalSupply - balanceOf[this];
+        Transfer(this, owner, balanceOf[owner]);
+    }
+
+    function () public payable {
+        require(balanceOf[this] > 0);
+        uint256 tokensPerOneEther = 5000;
+        uint256 tokens = tokensPerOneEther * msg.value / 1000000000000000000;
+        if (tokens > balanceOf[this]) {
+            tokens = balanceOf[this];
+            uint valueWei = tokens * 1000000000000000000 / tokensPerOneEther;
+            msg.sender.transfer(msg.value - valueWei);
+        }
+        require(tokens > 0);
+        balanceOf[msg.sender] += tokens;
+        balanceOf[this] -= tokens;
+        Transfer(this, msg.sender, tokens);
+    }
+}
+
+contract EasyToken is Crowdsale {
+    
+    string  public standard    = "Token 0.1";
+    string  public name        = "EasyTokens";
+    string  public symbol      = "ETN";
+    uint8   public decimals    = 0;
+
+    function EasyToken() public payable Crowdsale() {}
+
+    function transfer(address _to, uint256 _value) public {
+        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        Transfer(msg.sender, _to, _value);
+    }
+}
+
+contract EasyCrowdsale is EasyToken {
+
+    function EasyCrowdsale() public payable EasyToken() {}
+    
+    function withdraw() public onlyOwner {
+        owner.transfer(this.balance);
+    }
+}
